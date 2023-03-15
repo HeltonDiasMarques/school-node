8272e394e9fae7de563a0a1c077ca606ccd49ea9
HeltonDiasMarques <101150904+HeltonDiasMarques@users.noreply.github.com>
2023-03-15 17:42:56 -0300

    Add files via upload

---
+++ exercicio-1/hello-world.js
@@ -0,0 +1,11 @@
+console.log("Meu primeiro arquivo javascript no node!");
+
+const numero1 = 2;
+const numero2 = 2;
+
+const soma = () => {
+
+    return numero1 + numero2;
+};
+
+console.log ("O resultado de 2+2 é: ", soma())
